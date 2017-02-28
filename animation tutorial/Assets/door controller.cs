d3db5b474389e46afb2c0a5cddd076c495a5ac8a
Unknown <matt.debow@gmail.com>
2017-02-28 13:21:55 -0500

    added animation script

---
+++ animation tutorial/Assets/door controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorcontroller : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> (); 
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+
+		if (GUILayout.Button ("Open Door")) 
+		{
+			animator.SetInteger ("Door Close", 0);
+			animator.SetInteger ("Door OPen", 1);
+		}
+
+		if (GUILayout.Button ("Close Door"))
+		{
+			animator.SetInteger ("Door Close", 1);
+			animator.SetInteger ("Door OPen", 0);
+			
+		}
+
+	}
+}
+
