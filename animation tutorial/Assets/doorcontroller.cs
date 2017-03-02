08d8fb95359f699fe8a7bf322e0acbd9ac50cd32
Unknown <matt.debow@gmail.com>
2017-03-02 12:44:48 -0500

    added door controller

---
+++ animation tutorial/Assets/doorcontroller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorcontroller : MonoBehaviour 
+{
+
+	public Animator animator;
+	public Rect openDoorRectangle;
+	public Rect closeDoorRectangle;
+
+	void OnGUI () 
+	{
+
+		if(GUI.Button (openDoorRectangle, "Open Door")) 
+		{
+
+			animator.SetInteger ("Door Close", 0);
+			animator.SetInteger ("Door OPen", 1);
+		}
+
+		if(GUI.Button (closeDoorRectangle, "Close Door"))
+		{
+			animator.SetInteger ("Door Close", 1);
+			animator.SetInteger ("Door OPen", 0);
+
+				}
+			}
+		}	
